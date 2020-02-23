a9bd9f09244598b01ed9197baad535bc21fda2ad
Sookhee <s2018s38@e-mirim.hs.kr>
2020-02-24 00:27:03 +0900

    add 서채원 img, edit Main

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component{
         <div className="header">
           <ul>
             <li>
-              <div className="header-img">이미지</div>
+              <div className="header-img">로고</div>
               <NavLink to="/">APP &#38; ME</NavLink>
             </li>
             <li>
