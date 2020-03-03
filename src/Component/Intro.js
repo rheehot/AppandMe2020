b90eef805f7da99f32bab6e813f14cecfebdcb47
sokkanji <s2018s07@e-mirim.hs.kr>
2020-03-03 19:57:39 +0900

    ABOUT css 적용 및 F&A css 수정

--- src/Component/Intro.js
+++ src/Component/Intro.js
@@ -1,19 +1,19 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import '../styles/css/Intro.css';
 
-class Intro extends Component{
+class Intro extends Component {
 
-    render(){
+    render() {
         return (
             <div className="intro">
                 <div className="title">{this.props.title}</div>
                 <div className="notice">{this.props.notice1}</div>
                 <div className="notice">{this.props.notice2}</div>
-                <hr/>
+                <hr />
             </div>
         )
     }
-    
+
 }
 
 export default Intro;
