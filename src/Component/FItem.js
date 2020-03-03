7ad728d438b3542cb91a816f8978d0f3438f0525
sokkanji <s2018s07@e-mirim.hs.kr>
2020-03-03 16:16:07 +0900

    F&A css 적용

--- src/Component/FItem.js
+++ src/Component/FItem.js
@@ -1,35 +1,36 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import '../styles/css/FItem.css';
 
-class FA extends Component{
-    render(){
-        const {data} = this.props;
+class FA extends Component {
+    render() {
+        const { data } = this.props;
         console.log(data);
         return (
             <div className="frequently">
                 {
                     this.props.title === "min" ?
-                    (
-                        <details open>
-                            <summary>{this.props.title}</summary>
-                            {
-                            data.split('@').map( line => {
-                                return (<span>{line}<br/></span>)
-                            })
-                            }
-                        </details>
-                    ) : 
-                    (
-                        <details>
-                            <summary>{this.props.title}</summary>
-                            {
-                            data.split('@').map( line => {
-                                return (<span>{line}<br/></span>)
-                            })
-                            }
-                        </details>
-                    )
+                        (
+                            <details open>
+                                <summary>{this.props.title}</summary>
+                                {
+                                    data.split('@').map(line => {
+                                        return (<span>{line}<br /></span>)
+                                    })
+                                }
+                            </details>
+                        ) :
+                        (
+                            <details>
+                                <summary>{this.props.title}</summary>
+                                {
+                                    data.split('@').map(line => {
+                                        return (<span>{line}<br /></span>)
+                                    })
+                                }
+                            </details>
+                        )
                 }
+                <hr />
             </div>
         )
     }
