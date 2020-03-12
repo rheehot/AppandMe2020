a3f3f0c8921f9ea01732530282b9268ab562336f
Sookhee <s2018s38@e-mirim.hs.kr>
2020-03-12 21:43:08 +0900

    Intro, Apply, F&A css 수정

--- src/Component/Apply.js
+++ src/Component/Apply.js
@@ -1,6 +1,7 @@
 import React,{Component} from 'react';
 import Intro from './Intro';
 import '../styles/css/Apply.css';
+import '../styles/css/fonts.css';
 import {AppContext} from '../Context';
 
 class Apply extends Component {
@@ -15,32 +16,37 @@ class Apply extends Component {
             <div className="apply">
                 <Intro title="APPLY" notice1="앱앤미 지원을 결심하셨군요! 좋은 결과 있길 바라요^^" notice2="* 제출 후 수정이 불가하니 성심성의껏 작성해주시면 감사하겠습니다."/>
                 <form onSubmit={this.insertUser}>
-                    <table border="1">
+                    <table   className="font-regular">
                         <tr>
-                            <td>학번</td>
-                            <td>이름</td>
+                            <td className="tit font-medium">학번</td>
+                            <td className="temp"></td>
+                            <td className="tit font-medium">이름</td>
                         </tr>
                         <tr>
-                            <td><input type="text"></input></td>
-                            <td><input type="text"></input></td>
+                            <td><input type="text" placeholder="학번을 입력해 주세요"></input></td>
+                            <td className="temp"></td>
+                            <td><input type="text" placeholder="이름을 입력해 주세요"></input></td>
                         </tr>
                         <tr>
-                            <td colSpan="2">자기소개</td>
+                            <td colSpan="3" className="tit font-medium">자기소개</td>
                         </tr>
                         <tr>
-                            <td colSpan="2"><input type="text"></input></td>
+                            <td colSpan="3"><input type="text" placeholder="자기소개를 써주세요"></input></td>
                         </tr>
                         <tr>
-                            <td colSpan="2">지원동기</td>
+                            <td colSpan="3" className="tit font-medium" placeholder="지원동기를 써주세요">지원동기</td>
                         </tr>
                         <tr>
-                            <td colSpan="2"><input type="text"></input></td>
+                            <td colSpan="3"><input type="text" placeholder="자신을 다섯 글자로 표현해주세요"></input></td>
                         </tr>
                         <tr>
-                            <td colSpan="2">다섯글자</td>
+                            <td colSpan="3" className="tit font-medium">다섯글자</td>
                         </tr>
                         <tr>
-                            <td colSpan="2"><input type="text" placeholder="자신을 다섯 글자로 표현해주세요!"></input></td>
+                            <td colSpan="3"><input type="text" placeholder="자신을 다섯 글자로 표현해주세요!"></input></td>
+                        </tr>
+                        <tr className="tr-button">
+                            <td colSpan="3"><button type="submit" className="font-bold">제출하기</button></td>
                         </tr>
                     </table>
                 </form>
