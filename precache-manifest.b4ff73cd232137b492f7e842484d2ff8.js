43d45338566d26dbeb10bc6ae6a6852debc2948f
Sookhee <s2018s38@e-mirim.hs.kr>
2020-03-13 22:47:29 +0900

    Updates

---
+++ precache-manifest.b4ff73cd232137b492f7e842484d2ff8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9930cb2f522bcae61861369b008ffaf5",
+    "url": "/App-and-Me-Website2020/index.html"
+  },
+  {
+    "revision": "f1caca8a6597831dafe3",
+    "url": "/App-and-Me-Website2020/static/css/main.2fabb615.chunk.css"
+  },
+  {
+    "revision": "d104d070ca8935ca4ffa",
+    "url": "/App-and-Me-Website2020/static/js/2.c3da01ea.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/App-and-Me-Website2020/static/js/2.c3da01ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1caca8a6597831dafe3",
+    "url": "/App-and-Me-Website2020/static/js/main.ab213caf.chunk.js"
+  },
+  {
+    "revision": "e4238e09b2d3254c19d1",
+    "url": "/App-and-Me-Website2020/static/js/runtime-main.7801c68d.js"
+  },
+  {
+    "revision": "c09933ceee9f1126465baf4d2432eee6",
+    "url": "/App-and-Me-Website2020/static/media/NotoSansCJKkr-Black.c09933ce.otf"
+  },
+  {
+    "revision": "79d613fb8ec20fbb6bfcbf64e93cc9b2",
+    "url": "/App-and-Me-Website2020/static/media/NotoSansCJKkr-Bold.79d613fb.otf"
+  },
+  {
+    "revision": "a59504711a5c86e8e03b675bfeb0be76",
+    "url": "/App-and-Me-Website2020/static/media/NotoSansCJKkr-Medium.a5950471.otf"
+  },
+  {
+    "revision": "19b7d8d4011e8f88930022704558246d",
+    "url": "/App-and-Me-Website2020/static/media/NotoSansCJKkr-Regular.19b7d8d4.otf"
+  },
+  {
+    "revision": "33711969f1a530789aae6ca7838c09b4",
+    "url": "/App-and-Me-Website2020/static/media/app-and-me-logo.33711969.png"
+  },
+  {
+    "revision": "6dc7aa47a6c0c20aa347ee4480aaacbe",
+    "url": "/App-and-Me-Website2020/static/media/main-img.6dc7aa47.png"
+  }
+]);
